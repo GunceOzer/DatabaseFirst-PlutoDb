368ca9a66b40b62020f4288f4a4e0e9f6148a8ac
GunceOzer <guncedemirel.ozer@ogr.deu.edu.tr>
2021-06-03 17:34:01 +0300

    Add project files.

---
+++ DbFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dbContext = new PlutoDbContext(); // to access data from the database
+			var courses = dbContext.GetCourses();
+			
+			foreach(var c in courses)
+			{
+				Console.WriteLine(c.Title);
+			}
+
+			
+			var course = new Course();
+			course.Level = CourseLevel.Advanced; //i  created a enum  and used it 
+			
+			Console.WriteLine(course.Level);
+			Console.ReadKey();
+		}
+	}
+}
